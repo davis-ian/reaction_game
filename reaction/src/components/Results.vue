88cc0c2a85d685b4ba3451d4311daefed89cc08b
davis-ian <iandavis6481@gmail.com>
2024-05-30 08:26:56 -0500

    key clean test

--- reaction/src/components/Results.vue
+++ reaction/src/components/Results.vue
@@ -5,25 +5,22 @@
 
 <script>
 export default {
-    props: ['scores'],
-    data() {
-        return {
-            rank: null
-            key1: "9178b3a4-1f97-43d7-814e-86cb232f5b6f security 9178b3a4-1f97-43d7-814e-86cb232f5b6f"
-            key2: " security "
-        }
-
-    },
-    mounted() {
-        if (this.scores < 250) {
-            this.rank = "Ninja Fingers"
-        } else if (this.scores < 400) {
-            this.rank = "Pretty Quick Reflexes"
-        } else {
-            this.rank = "Snail Pace..."
-        }
+  props: ['scores'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.scores < 250) {
+      this.rank = 'Ninja Fingers';
+    } else if (this.scores < 400) {
+      this.rank = 'Pretty Quick Reflexes';
+    } else {
+      this.rank = 'Snail Pace...';
     }
-}
+  },
+};
 </script>
 
 <style>
